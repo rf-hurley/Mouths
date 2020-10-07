4ebef3d9267fd946fccad6582aa2a7cd2dda2ca9
rf-hurley <70685246+rf-hurley@users.noreply.github.com>
2020-10-07 14:00:03 -0700

    Add files via upload

---
+++ mouth.js
@@ -0,0 +1,37 @@
+class Mouths {
+    constructor(){
+        this.shapes = [line, oval, semicircle];
+        this.name = 'mouth';
+        this.identity = 'you';
+        this.openSprite = "animations/mouth-open.png";
+        this.shoutSprite = "animations/mouth-shout.png";
+        this.smileSprite = "animations/mouth-smile.png";
+        this.frownSprite = "animations/mouth-frown.png";
+    }
+
+    talk(){
+    }
+
+    expressYourself(){
+
+        // open(){
+        // }
+
+        // shout(){
+        // }
+    
+        // smile(){
+        // }
+    
+        // frown(){
+        // }
+    
+    }
+
+    tellTruth(){
+    }
+
+    tellLie(){
+    }
+
+}
